24b56e0d1d44a6b6f2152a1ad0a23542e6e00a8e
max-blabla <74357554+max-blabla@users.noreply.github.com>
2021-06-03 10:59:11 +0800

    Update main.cpp

--- src/Test_Train/hpp/main.cpp
+++ src/Test_Train/hpp/main.cpp
@@ -105,8 +105,9 @@ void Query_Ticket(){
     delete cmdQue;
 }
 int main(){
-    Backend::Main OP;
+    //Backend::Main OP;
     //其中第一个参数为输入流，第二个参数为输出流
-    OP.Run(std::cin,std::cout);
-    return 0;
+    //OP.Run(std::cin,std::cout);
+    //return 0;
+    Query_Ticket();
 }

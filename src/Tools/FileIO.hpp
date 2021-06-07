99ec90bb46b01e3d4605dfc7ddad1d05d8369837
max-blabla <74357554+max-blabla@users.noreply.github.com>
2021-06-07 14:24:56 +0800

    Update FileIO.hpp

--- src/Tools/FileIO.hpp
+++ src/Tools/FileIO.hpp
@@ -1,7 +1,3 @@
-//
-// Created by Ubospica on 2021/5/14.
-//
-
 #ifndef TICKETSYSTEM_2021_FILEIO_HPP
 #define TICKETSYSTEM_2021_FILEIO_HPP
 
@@ -14,15 +10,15 @@
 
 #include "Exception.hpp"
 #include "List.hpp"
-#include "map_for_cache/map_cache.hpp"
+#include "map_cache.hpp"
 
 namespace Ticket {
-	
-	/**
-	 * file IO class
-	 *
-	 * maintains three things : a filestream, a read&write pointer, and a flag showing whether the file is opened for the first time
-	 */
+
+    /**
+     * file IO class
+     *
+     * maintains three things : a filestream, a read&write pointer, and a flag showing whether the file is opened for the first time
+     */
 //	class FileIONoCache {
 //	//protected:
 //	public:
@@ -170,284 +166,284 @@ namespace Ticket {
 //		}
 //
 //	};
-	
-	class FileIONoCache {
-	//protected:
-	public:
-		std::FILE *fs = nullptr;
-		bool firstOpen = false;
-		std::string name;
 
-	public:
-		/**
-		 * @brief position constants
-		 */
-		enum Pos {
-			END = -1, BEG = 0,
-		}; //CUR = -2,
+    class FileIONoCache {
+        //protected:
+    public:
+        std::FILE *fs = nullptr;
+        bool firstOpen = false;
+        std::string name;
 
-		FileIONoCache() = default;
+    public:
+        /**
+         * @brief position constants
+         */
+        enum Pos {
+            END = -1, BEG = 0,
+        }; //CUR = -2,
 
-		/**
-		 * constructor
-		 * @param name file name
-		 */
-		explicit FileIONoCache(const std::string &name) {
-			open(name);
-		}
+        FileIONoCache() = default;
 
-		FileIONoCache& operator=(FileIONoCache &&another) = default;
+        /**
+         * constructor
+         * @param name file name
+         */
+        explicit FileIONoCache(const std::string &name) {
+            open(name);
+        }
 
-		void open(const std::string &name) {
-			if (fs != nullptr) {
-				fclose(fs);
-				clearerr(fs);
-			}
-			this -> name = name;
-			fs = fopen(name.c_str(), "rb+");
-			if (fs == nullptr) {
-				fclose(fopen(name.c_str(), "a"));
-				fs = fopen(name.c_str(), "rb+");
-				if (fs == nullptr) {
-					throw RuntimeError("File system error");
-				}
-				firstOpen = true;
-			}
-		}
+        FileIONoCache& operator=(FileIONoCache &&another) = default;
 
-		/**
-		 * check if the file is opened for the first time
-		 * @return true when is opened first; false otherwise
-		 */
-		bool isFirstOpen() const {
-			return firstOpen;
-		}
+        void open(const std::string &name) {
+            if (fs != nullptr) {
+                fclose(fs);
+                clearerr(fs);
+            }
+            this -> name = name;
+            fs = fopen(name.c_str(), "rb+");
+            if (fs == nullptr) {
+                fclose(fopen(name.c_str(), "a"));
+                fs = fopen(name.c_str(), "rb+");
+                if (fs == nullptr) {
+                    throw RuntimeError("File system error");
+                }
+                firstOpen = true;
+            }
+        }
 
-		/**
-		 * read value from fstream
-		 * @tparam T
-		 * @param pos Pos::CUR (-2) or >= 0
-		 * @param cur value
-		 */
-		template <typename T>
-		void read(int pos, T &cur) {
-			if(pos >= 0){ //pos == CUR == -2
-				fseek(fs, pos, SEEK_SET);
-			}
-			fread(reinterpret_cast<void*>(&cur), sizeof(cur), 1, fs);
-		}
+        /**
+         * check if the file is opened for the first time
+         * @return true when is opened first; false otherwise
+         */
+        bool isFirstOpen() const {
+            return firstOpen;
+        }
 
-		/**
-		 * write value to fs
-		 * @tparam T
-		 * @param pos >=0 or Pos::END(-1) or Pos::CUR (-2)
-		 * @param cur value
-		 */
-		template <typename T>
-		void write(int pos, const T &cur) {
-			if(pos >= 0){
-				fseek(fs, pos, SEEK_SET);
-			}
-			else if(pos == Pos::END) {
-				fseek(fs, 0, SEEK_END);
-			}
+        /**
+         * read value from fstream
+         * @tparam T
+         * @param pos Pos::CUR (-2) or >= 0
+         * @param cur value
+         */
+        template <typename T>
+        void read(int pos, T &cur) {
+            if(pos >= 0){ //pos == CUR == -2
+                fseek(fs, pos, SEEK_SET);
+            }
+            fread(reinterpret_cast<void*>(&cur), sizeof(cur), 1, fs);
+        }
+
+        /**
+         * write value to fs
+         * @tparam T
+         * @param pos >=0 or Pos::END(-1) or Pos::CUR (-2)
+         * @param cur value
+         */
+        template <typename T>
+        void write(int pos, const T &cur) {
+            if(pos >= 0){
+                fseek(fs, pos, SEEK_SET);
+            }
+            else if(pos == Pos::END) {
+                fseek(fs, 0, SEEK_END);
+            }
 //			int tmp = sizeof(cur);
 //			std::cerr << "write " << name  << ' ' << cur << ' ' << sizeof(cur) << '\n';
 //			std::cerr << "before = " << fs.tellp();
-			fwrite(reinterpret_cast<const void*>(&cur), sizeof(cur), 1, fs);
+            fwrite(reinterpret_cast<const void*>(&cur), sizeof(cur), 1, fs);
 //			std::cerr << " after = " << fs.tellp() << '\n';
-		}
+        }
 
-		void write(int pos, void *val, int size) {
-			if(pos >= 0){
-				fseek(fs, pos, SEEK_SET);
-			}
-			else if(pos == Pos::END) {
-				fseek(fs, 0, SEEK_END);
-			}
-			fwrite(val, size, 1, fs);
-		}
+        void write(int pos, void *val, int size) {
+            if(pos >= 0){
+                fseek(fs, pos, SEEK_SET);
+            }
+            else if(pos == Pos::END) {
+                fseek(fs, 0, SEEK_END);
+            }
+            fwrite(val, size, 1, fs);
+        }
 
-		/**
-		 * move position pointer
-		 * @param pos >=0 or Pos::END
-		 */
-		void movePos(int pos) {
-			if (pos >= 0) {
-				fseek(fs, pos, SEEK_CUR);
-			}
-			else if (pos == Pos::END) {
-				fseek(fs, 0, SEEK_END);
-			}
-		}
+        /**
+         * move position pointer
+         * @param pos >=0 or Pos::END
+         */
+        void movePos(int pos) {
+            if (pos >= 0) {
+                fseek(fs, pos, SEEK_CUR);
+            }
+            else if (pos == Pos::END) {
+                fseek(fs, 0, SEEK_END);
+            }
+        }
 
-		/**
-		 * move pointer forward
-		 * @param pos the distance that the pointer is moved forward for
-		 */
-		void moveForward(int pos) {
-			fseek(fs, pos, SEEK_CUR);
-		}
+        /**
+         * move pointer forward
+         * @param pos the distance that the pointer is moved forward for
+         */
+        void moveForward(int pos) {
+            fseek(fs, pos, SEEK_CUR);
+        }
 
-		/**
-		 * show current pointer
-		 * @return
-		 */
-		size_t tellPos() {
-			return ftell(fs);
-		}
+        /**
+         * show current pointer
+         * @return
+         */
+        size_t tellPos() {
+            return ftell(fs);
+        }
 
-		/**
-		 * close the fstream
-		 */
-		void close() {
-			if (fs != nullptr) {
-				fclose(fs);
-				fs = nullptr;
-			}
-		}
+        /**
+         * close the fstream
+         */
+        void close() {
+            if (fs != nullptr) {
+                fclose(fs);
+                fs = nullptr;
+            }
+        }
 
-		void clear() {
+        void clear() {
 //			std::cerr << "clear\n";
-			fclose(fs);
-			fs = fopen(name.c_str(), "w+");
-			firstOpen = true;
+            fclose(fs);
+            fs = fopen(name.c_str(), "w+");
+            firstOpen = true;
 //			bool tmp = fs.fail();
-		}
+        }
 
-		~FileIONoCache() {
-			close();
-		}
+        ~FileIONoCache() {
+            close();
+        }
 
-	};
+    };
 
-	/**
-	 * FileIO with LRU cache
-	 */
-	class FileIO : public FileIONoCache {
-	// public:
-	// 	using FileIONoCache::FileIONoCache;
-	// };
+    /**
+     * FileIO with LRU cache
+     */
+    class FileIO : public FileIONoCache {
+         public:
+         	using FileIONoCache::FileIONoCache;
+         };
 
-	protected:
-		struct CacheNode {
-			void *value = nullptr;
-			int pos = 0, size = 0;
-			bool isModifyed = false;
-			CacheNode() = default;
-			explicit CacheNode(void *value, int pos, int size) : value(value), pos(pos), size(size) {}
-			~CacheNode() {
+   /* protected:
+        struct CacheNode {
+            void *value = nullptr;
+            int pos = 0, size = 0;
+            bool isModifyed = false;
+            CacheNode() = default;
+            explicit CacheNode(void *value, int pos, int size) : value(value), pos(pos), size(size) {}
+            ~CacheNode() {
 //				if (value != nullptr) {
 //					operator delete(value);
 //					value = nullptr;
 //				}
-			}
-			friend std::ostream& operator << (std::ostream &os, CacheNode one) {
-				os << one.value;
-				if (one.value != nullptr) os << ' ' << *(int*)one.value;
-				os << " pos = " << one.pos << " sz = " << one.size << " ismo = " << one.isModifyed;
-				return os;
-			}
-		};
-		List<CacheNode> cache;
-		map<int, List<CacheNode>::Node*> cacheIndex;
-		int cacheCnt = 0;
-		static const int cacheSize = 1000;
+            }
+            friend std::ostream& operator << (std::ostream &os, CacheNode one) {
+                os << one.value;
+                if (one.value != nullptr) os << ' ' << *(int*)one.value;
+                os << " pos = " << one.pos << " sz = " << one.size << " ismo = " << one.isModifyed;
+                return os;
+            }
+        };
+        List<CacheNode> cache;
+        map<int, List<CacheNode>::Node*> cacheIndex;
+        int cacheCnt = 0;
+        static const int cacheSize = 1000;
 
-		template<typename T>
-		void _insertCache(int pos, T *value) {
-			auto res = cache.insert(cache.head, CacheNode(static_cast<void*>(value), pos, sizeof(T)));
-			cacheIndex.insert(make_pair(pos, res));
-			++cacheCnt;
-			if (cacheCnt > cacheSize) {
-				auto endNode = (cache.end) -> prev;
-				_popNode(endNode);
-				--cacheCnt;
-			}
-		}
+        template<typename T>
+        void _insertCache(int pos, T *value) {
+            auto res = cache.insert(cache.head, CacheNode(static_cast<void*>(value), pos, sizeof(T)));
+            cacheIndex.insert(make_pair(pos, res));
+            ++cacheCnt;
+            if (cacheCnt > cacheSize) {
+                auto endNode = (cache.end) -> prev;
+                _popNode(endNode);
+                --cacheCnt;
+            }
+        }
 
-		void _popNode(List<CacheNode>::Node *oneNode) {
-			if ((oneNode -> value).isModifyed) {
-				FileIONoCache::write((oneNode -> value).pos, oneNode -> value.value, oneNode -> value.size);
-			}
-			cacheIndex.erase(cacheIndex.find(oneNode -> value.pos));
-			operator delete((oneNode -> value).value);
-			cache.erase(oneNode);
-		}
+        void _popNode(List<CacheNode>::Node *oneNode) {
+            if ((oneNode -> value).isModifyed) {
+                FileIONoCache::write((oneNode -> value).pos, oneNode -> value.value, oneNode -> value.size);
+            }
+            cacheIndex.erase(cacheIndex.find(oneNode -> value.pos));
+            operator delete((oneNode -> value).value);
+            cache.erase(oneNode);
+        }
 
-		void _moveCache(List<CacheNode>::Node *oneNode) {
-			cache.eraseNoDelete(oneNode);
-			cache.insertNode(cache.head, oneNode);
-		}
+        void _moveCache(List<CacheNode>::Node *oneNode) {
+            cache.eraseNoDelete(oneNode);
+            cache.insertNode(cache.head, oneNode);
+        }
 
-	public:
-		FileIO() : FileIONoCache(), cache(), cacheIndex() { }
-		explicit FileIO (const std::string &name) : FileIONoCache(name), cache(), cacheIndex() { }
-		~FileIO() {
-			auto current = cache.head -> next;
-			while (current != cache.end) {
-				auto tmp = current;
-				current = current -> next;
-				_popNode(tmp);
-			}
-		}
+    public:
+        FileIO() : FileIONoCache(), cache(), cacheIndex() { }
+        explicit FileIO (const std::string &name) : FileIONoCache(name), cache(), cacheIndex() { }
+        ~FileIO() {
+            auto current = cache.head -> next;
+            while (current != cache.end) {
+                auto tmp = current;
+                current = current -> next;
+                _popNode(tmp);
+            }
+        }
 
 
-		template <typename T>
-		void read(int pos, T &cur)  {
-			int realPos = pos;
+        template <typename T>
+        void read(int pos, T &cur)  {
+            int realPos = pos;
 //			if (pos < 0) realPos = fs.tellg();
-			auto it = cacheIndex.find(realPos);
-			if (it == cacheIndex.end()) {
-				//not found
-				T *ptr = static_cast<T*>(operator new(sizeof(T)));
-				FileIONoCache::read(realPos, *ptr);
-				cur = *ptr;
-				_insertCache(realPos, ptr);
-			}
-			else {
-				cur = *static_cast<T*>((it -> second -> value).value);
-				_moveCache(it -> second);
-			}
+            auto it = cacheIndex.find(realPos);
+            if (it == cacheIndex.end()) {
+                //not found
+                T *ptr = static_cast<T*>(operator new(sizeof(T)));
+                FileIONoCache::read(realPos, *ptr);
+                cur = *ptr;
+                _insertCache(realPos, ptr);
+            }
+            else {
+                cur = *static_cast<T*>((it -> second -> value).value);
+                _moveCache(it -> second);
+            }
 //			cache.print();
-		}
+        }
 
-		/**
-		 * write value to fs
-		 * @tparam T
-		 * @param pos >=0 or Pos::END(-1) or Pos::CUR (-2)
-		 * @param cur value
-		 */
-		template <typename T>
-		void write(int pos, const T &cur) {
-			int realPos = pos;
+        *//**
+         * write value to fs
+         * @tparam T
+         * @param pos >=0 or Pos::END(-1) or Pos::CUR (-2)
+         * @param cur value
+         *//*
+        template <typename T>
+        void write(int pos, const T &cur) {
+            int realPos = pos;
 //			if (pos == Pos::CUR) realPos = fs.tellg();
-			if (pos == Pos::END) {
-				FileIONoCache::write(pos, cur);
-				return;
-			}
-			auto it = cacheIndex.find(realPos);
-			if (it == cacheIndex.end()) {
-				FileIONoCache::write(realPos, cur);
-				return;
-			}
-			else {
-				auto &cacheNode = it -> second -> value;
-				*static_cast<T*>(cacheNode.value) = cur;
-				cacheNode.isModifyed = true;
-			}
-		}
-		void clear() {
-			auto current = cache.head -> next;
-			while (current != cache.end) {
-				auto tmp = current;
-				current = current -> next;
-				_popNode(tmp);
-			}
-			FileIONoCache::clear();
-			cacheCnt = 0;
-		}
-	};
+            if (pos == Pos::END) {
+                FileIONoCache::write(pos, cur);
+                return;
+            }
+            auto it = cacheIndex.find(realPos);
+            if (it == cacheIndex.end()) {
+                FileIONoCache::write(realPos, cur);
+                return;
+            }
+            else {
+                auto &cacheNode = it -> second -> value;
+                *static_cast<T*>(cacheNode.value) = cur;
+                cacheNode.isModifyed = true;
+            }
+        }
+        void clear() {
+            auto current = cache.head -> next;
+            while (current != cache.end) {
+                auto tmp = current;
+                current = current -> next;
+                _popNode(tmp);
+            }
+            FileIONoCache::clear();
+            cacheCnt = 0;
+        }
+    };*/
 }
 
 

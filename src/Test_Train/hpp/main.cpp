64ab7aedc922cb92f94faf426e8133ce91129544
max-blabla <74357554+max-blabla@users.noreply.github.com>
2021-06-08 21:29:45 +0800

    Update main.cpp

--- src/Test_Train/hpp/main.cpp
+++ src/Test_Train/hpp/main.cpp
@@ -3,13 +3,13 @@
 #include<string>
 using namespace std;
 int main(){
- //   std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
+    std::ios::sync_with_stdio(0), std::cin.tie(0), std::cout.tie(0);
 //	freopen("a.in", "r", stdin);
 //	freopen("a.out", "w", stdout);
 //	for (int i = 1; i <= 10; ++i) {
     Backend::Main OP;
 //    其中第一个参数为输入流，第二个参数为输出流
-    OP.Run();
+    OP.Run(std::cin,std::cout);
 //	}
     return 0;
 }
